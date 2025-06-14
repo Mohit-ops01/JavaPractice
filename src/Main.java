6374ca31651fad1caaaa116bfaeefa28c47c5c52
Mohit <sharmamohit98551@gmail.com>
2025-06-15 02:44:12 +0530

    Day 1 - Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //hello , this is my first java program
+        /*
+        alright
+        this
+        is
+        a
+        multiline
+        comment
+        */
+       System.out.println("Hello World");
+       System.out.print("My name is Mohit\n");
+    }
+}
---
+++ src/main2.java
@@ -0,0 +1,8 @@
+public class main2 {
+    public static void main(String[] args){
+        /* this is my practice assignment of main.java */
+        System.out.println("im a confident coder");
+        System.out.print("im a consistent coder\n");
+        System.out.println("im becoming unbeatable");
+    }
+}
